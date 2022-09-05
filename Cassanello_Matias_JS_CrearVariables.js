7c07aa02d58e9312966ca2bcf4069f3e965e10d4
MCassa99 <m_cassa@icloud.com>
2022-09-05 20:02:38 -0300

    Update Cassanello_Matias_JS_CrearVariables.js

--- Cassanello_Matias_JS_CrearVariables.js
+++ Cassanello_Matias_JS_CrearVariables.js
@@ -1,8 +1,12 @@
 var altura;
 var edad;
-if (altura >= 1 && edad > 10)
-{
-    console.log("Aprovado para Subir!");
+
+// Recaudo la informacion de altura y edad...
+
+if (altura >= 1 && edad > 10) {
+    console.log("Aprovado para Subir: ");
+} else {
+    console.log("NO Aprovado para Subir: ");
 }
 /* A las variables se les asigna la respectiva altura y edad,
 si la altura es mayor o igual a 1 metro y la edad mayor a 10,
