41003408539eb8b8899d8a04b1488a22e1982142
MCassa99 <m_cassa@icloud.com>
2022-09-05 20:30:47 -0300

    Modificacion Final

--- Cassanello_Matias_JS_CrearVariables.js
+++ Cassanello_Matias_JS_CrearVariables.js
@@ -1,13 +1,13 @@
-var altura;
-var edad;
+var altura = 150;
+var edad = 11;
 
 // Recaudo la informacion de altura y edad...
-
-if (altura >= 1 && edad > 10) {
+if (altura >= 100 && edad > 10) {
     console.log("Aprovado para Subir: ");
 } else {
     console.log("NO Aprovado para Subir: ");
 }
+
 /* A las variables se les asigna la respectiva altura y edad,
 si la altura es mayor o igual a 1 metro y la edad mayor a 10,
 entonces la persona podra subir a la atracción.*/
