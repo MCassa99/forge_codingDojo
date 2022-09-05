b8f19b3129f49c8ff4d470eaabf9a6a781c1d4b1
MCassa99 <m_cassa@icloud.com>
2022-09-05 19:29:23 -0300

    Primer Ejercicio
    CLOUD COMPUTING - PRE-BOOTCAMP SPANISH
    
    Desafío de código: crear variables

---
+++ Cassanello_Matias_JS_CrearVariables.js
@@ -0,0 +1,5 @@
+var altura;
+var edad;
+/* A las variables se les asigna la respectiva altura y edad,
+si la altura es mayor o igual a 1 metro y la edad mayor a 10,
+entonces la persona podra subir a la atracción.*/
