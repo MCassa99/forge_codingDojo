0cbfe373111e6b14103cc2e2f06dab1937da9d24
MCassa99 <m_cassa@icloud.com>
2022-09-06 19:25:36 -0300

    Create Cassanello_Greetings.js

---
+++ Cassanello_Greetings.js
@@ -0,0 +1,9 @@
+function greet (name, hora) {
+        if (name == 'Count Dooku') {
+        console.log('¡Voy por ti, Dooku!');
+    } else {
+        console.log('¡Buen día, ' + name +'! Son las ' + hora + 'hs');
+    }
+}
+
+greet("Count Dooku", 16.15);
