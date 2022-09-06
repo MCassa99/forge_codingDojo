b12219511b94b650a9d31d35f0c66585f584d0d3
MCassa99 <m_cassa@icloud.com>
2022-09-06 20:19:15 -0300

    Create Cassanello_Matias_JSPredicción.js

---
+++ Cassanello_Matias_JSPredicción.js
@@ -0,0 +1,8 @@
+/* *Predicción 1: * */
+// console.log indicara Nací en 1980
+
+/* *Predicción 2: * */
+// console.log indicara Nací en 1980
+
+/* *Predicción 3: * */
+// console.log indicara ¡Sumando números!, num1 es: 10, num2 es: 20, 30
