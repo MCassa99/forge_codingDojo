724d6d90e52dba903ac4cf4d83d1fb06fe79c60b
MCassa99 <m_cassa@icloud.com>
2022-09-09 13:31:05 -0300

    Update Cassanello_Greetings.js

--- Cassanello_Greetings.js
+++ Cassanello_Greetings.js
@@ -2,11 +2,11 @@ var hora = new Date();
 
 function greet (name, actualHora) {
         if (name == 'Count Dooku') {
-        console.log('¡Voy por ti, Dooku!');
+        console.log('¡Voy por ti, Dooku! Por cierto, son las ' + actualHora + 'hs');
     } else {
         console.log('¡Buen día, ' + name +'! Son las ' + actualHora + 'hs');
     }
 }
 
 var actualHora = hora.getHours() + ':' + hora.getMinutes();
-greet("Count Doku", actualHora);
+greet("Count Dooku", actualHora);
